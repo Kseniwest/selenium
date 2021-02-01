126ab8047338e8034cc108cc6b940fbbae28671f
Kseniia <56968790+Kseniwest@users.noreply.github.com>
2021-02-01 15:08:16 +0300

    Create send_keys(file_path).py

---
+++ send_keys(file_path).py
@@ -0,0 +1,45 @@
+'''
+В этом задании в форме регистрации требуется загрузить текстовый файл.
+
+Напишите скрипт, который будет выполнять следующий сценарий:
+
+Открыть страницу http://suninjuly.github.io/file_input.html
+Заполнить текстовые поля: имя, фамилия, email
+Загрузить файл. Файл должен иметь расширение .txt и может быть пустым
+Нажать кнопку "Submit"
+'''
+
+import os
+from selenium import webdriver
+import time
+
+try:
+    link = "http://suninjuly.github.io/file_input.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    input1 = browser.find_element_by_xpath("//input[@placeholder='Enter first name']") # поиск по плейсхолдеру
+    input1.send_keys("Iv")
+    input2 = browser.find_element_by_xpath("//input[@placeholder='Enter last name']")
+    input2.send_keys("Pe")
+    input4 = browser.find_element_by_xpath("//input[@placeholder='Enter email']")
+    input4.send_keys("Rus")
+
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    file_name = "file.txt"
+    file_path = os.path.join(current_dir, file_name)
+    element = browser.find_element_by_css_selector("[type='file']")
+    element.send_keys(file_path)
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
