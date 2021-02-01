bbc2fc6d65290fc0c16a9d36dd6adc28dca059e8
Kseniia <56968790+Kseniwest@users.noreply.github.com>
2021-02-01 17:17:42 +0300

    Create explicit_wait2.py

---
+++ explicit_wait2.py
@@ -0,0 +1,58 @@
+'''
+Попробуем теперь написать программу, которая будет бронировать нам дом для отдыха по строго заданной цене. Более высокая цена нас не устраивает, а по более низкой цене объект успеет забронировать кто-то другой.
+
+В этой задаче вам нужно написать программу, которая будет выполнять следующий сценарий:
+
+Открыть страницу http://suninjuly.github.io/explicit_wait2.html
+Дождаться, когда цена дома уменьшится до $100 (ожидание нужно установить не меньше 12 секунд)
+Нажать на кнопку "Book"
+Решить уже известную нам математическую задачу (используйте ранее написанный код) и отправить решение
+'''
+
+
+
+
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import time
+
+link = "http://suninjuly.github.io/explicit_wait2.html"
+
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
+    '''синтаксис для text_to_be_present_in_element  ((By.ID, "здесь пишем ID"), "здесь текст")'''
+
+
+    button = browser.find_element_by_id("book")
+    button.click()
+
+
+
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+    input1 = browser.find_element_by_class_name("form-control")
+    input1.send_keys(y)
+
+    button = browser.find_element_by_id("solve")
+    button.click()
+
+
+
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
